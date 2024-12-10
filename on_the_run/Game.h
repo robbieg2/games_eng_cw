326fef833386fdfa5e2e9a4a75c650f7960e5bdb
FinDaGardiner <157618651+FinDaGardiner@users.noreply.github.com>
2024-12-10 22:27:27 +0000

    Update Game.h

--- on_the_run/Game.h
+++ on_the_run/Game.h
@@ -6,29 +6,35 @@
 class Game
 {
 public:
-	Game();
-	void	run();
+    Game();
+    void run();
 
 private:
-	void	processEvents();
-	void	update(sf::Time deltaTime);
-	void	render();
+    void processEvents();
+    void update(sf::Time deltaTime);
+    void render();
 
 private:
-	sf::RenderWindow mWindow;
-	sf::Time TimePerFrame = sf::seconds(1.f / 60.f);
+    sf::RenderWindow mWindow;
+    sf::Time TimePerFrame = sf::seconds(1.f / 60.f);
 
-	sf::Texture mBackgroundTexture;
-	sf::Sprite mBackgroundSprite;
+    sf::Texture mBackgroundTexture;
+    sf::Sprite mBackgroundSprite;
 
-	std::vector<Police> policeCars;
+    std::vector<Police> policeCars;
 
-	Player mPlayer;
-	Police mPolice;
+    Player mPlayer;
+    Police mPolice;
+
+    sf::View mView; // View for camera
+    sf::FloatRect mapBounds; // Map bounds for clamping
 };
+
+
+
 
 int main()
 {
-	Game Game;
-	Game.run();
+    Game Game;
+    Game.run();
 }
