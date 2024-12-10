4e4b9d50c1678058aa1eeb0ce11158e4f5331a12
FinDaGardiner <157618651+FinDaGardiner@users.noreply.github.com>
2024-12-10 22:28:27 +0000

    Update Player.h

--- on_the_run/Player.h
+++ on_the_run/Player.h
@@ -3,44 +3,43 @@
 #include <SFML/System/Vector2.hpp>
 #include <SFML/Graphics.hpp>
 
-
 class Player {
 public:
-	Player();
+    Player();
 
-	void handlePlayerInput(sf::Keyboard::Key key, bool isPressed);
+    void handlePlayerInput(sf::Keyboard::Key key, bool isPressed);
 
-	void updateAnimationMovement();
+    void updateAnimationMovement();
 
-	void updateAccelleration(sf::Time deltaTime);
+    void updateAccelleration(sf::Time deltaTime);
 
-	void updateDeceleration(sf::Time deltaTime);
+    void updateDeceleration(sf::Time deltaTime);
 
-	void updateMovement(sf::Time deltaTime);
+    void updateMovement(sf::Time deltaTime);
 
-	void move(sf::Time deltaTime);
+    void move(sf::Time deltaTime);
 
-	sf::Sprite& getSprite() { return mPlayer; }
+    sf::Sprite& getSprite() { return mPlayer; }
 
-	sf::Vector2f getPosition() const { return mPlayer.getPosition(); }
+    sf::Vector2f getPosition() const { return mPlayer.getPosition(); }
 
 private:
-	sf::Sprite mPlayer;
+    sf::Sprite mPlayer;
 
-	sf::Texture mPlayerTexture;
+    sf::Texture mPlayerTexture;
 
-	float movementSpeed;
-	float maxSpeedForward;
-	float maxSpeedBackwards;
+    float movementSpeed;
+    float maxSpeedForward;
+    float maxSpeedBackwards;
 
-	bool pIsMovingUp;
-	bool pIsMovingDown;
-	bool pIsMovingLeft;
-	bool pIsMovingRight;
+    bool pIsMovingUp;
+    bool pIsMovingDown;
+    bool pIsMovingLeft;
+    bool pIsMovingRight;
 
-	int currentFrame;
-	int frameCounter;
+    int currentFrame;
+    int frameCounter;
 
-	sf::Vector2f movement;
-	sf::Vector2f rotatedMovement;
+    sf::Vector2f movement;
+    sf::Vector2f rotatedMovement;
 };
